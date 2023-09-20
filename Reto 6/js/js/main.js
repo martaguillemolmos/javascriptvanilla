a41a3918bbaa8241785a3e7f3fb403178d051a39
martaguillemolmos <martetaguillem@gmail.com>
2023-09-20 18:37:49 +0200

    Reto 6 completado, aquí lo más complejo ha sido el operación que hay que realizar para obtener el IVA.

---
+++ Reto 6/js/js/main.js
@@ -0,0 +1,10 @@
+// Lee un número por teclado que pida el precio de un producto (puede tener decimales) y
+// calcule el precio final con IVA. El IVA será una constante que sera del 21%.
+
+
+let a = parseFloat(prompt("Introduce valor de radio"));
+
+let b = ((a)*(0.21))+ (a);
+
+alert (b);
+
