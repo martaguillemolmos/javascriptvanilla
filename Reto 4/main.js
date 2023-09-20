004e1c782335efd874b38be5f2206e13069a18e0
martaguillemolmos <martetaguillem@gmail.com>
2023-09-20 18:04:56 +0200

    Realizamos Reto 4, importante, utilizar parseFloat para poder introducir números que no sean enteros y, determinaremos la variable pi, antes del incio del ejercicio.

---
+++ Reto 4/main.js
@@ -0,0 +1,20 @@
+// Haz una aplicación que calcule el área de un círculo (pi*R2). El radio se pedirá por teclado
+// (recuerda pasar de String a double con parseDouble). Usa la constante PI.
+
+let pi = 3.1416;
+let a = parseFloat(prompt("Introduce valor de radio"));
+
+let b = (a)*(a)*(pi);
+
+
+console.log (b);
+
+// Utilizamos el ejemplo que hemos visto en clase con parseInt:
+
+// let primero = "123";
+
+// let segundo = "321";
+
+// let resultado = parseInt(primero) + parseInt(segundo);
+
+// console.log(resultado);
