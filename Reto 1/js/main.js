f936f0d0173f637e9633dd7c37c1d0a4dcc8fd39
martaguillemolmos <martetaguillem@gmail.com>
2023-09-20 16:30:23 +0200

    Subimos Reto 1 terminado y revisado

--- Reto 1/js/main.js
+++ Reto 1/js/main.js
@@ -1,3 +1,6 @@
+// Declara 2 variables numéricas (con el valor que desees), he indica cual es mayor de los
+// dos. Si son iguales indicarlo también. Ves cambiando los valores para comprobar que
+// funciona.
 
 let a = 2;
 let b = 5;
---
+++ Reto 2/js/main.js
@@ -0,0 +1,15 @@
+
+let a = 2;
+let b = 5;
+
+
+    if (a > b){
+        console.log ("variable A es mayor");
+     } else {
+        console.log ("variable B es mayor");
+    
+     }
+
+
+
+
